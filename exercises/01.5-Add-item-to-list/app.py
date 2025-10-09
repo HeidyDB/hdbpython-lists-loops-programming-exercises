92a01ea9c7b2df72363703d15d3c4afd3e8e0b08
Heidy Díaz <heidydiazbarrios@gmail.com>
2025-10-09 20:31:33 +0000

    loops2

--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -1,5 +1,16 @@
 # Remember to import random function here
+#add 10 random integer to my list 
 
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
+
+import random
+nueva_list  =[]
+
+for i in range(1, 11):
+    range_numb = random.randint(1,1000)
+    my_list.append(range_numb)
+   
+
+print (my_list)
--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,3 +1,8 @@
 names = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 # Your code here
+names[1]= 'Steven'
+names[len(names)-1]='Pepe' #la ultima posision
+names[0]= names[2]+names[4] # concatenar 
+for i in range(len(names)-1, -1, -1): #imprimir la lista en reverso 
+    print(names[i])
