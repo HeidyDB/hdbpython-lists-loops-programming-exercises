c3420e27a7e13f62a44c4a51f20a176b24cc4475
Heidy Díaz <heidydiazbarrios@gmail.com>
2025-10-15 14:57:51 +0000

    loop ex 4# Please enter the commit message for your changes. Lines starting

--- exercises/04-mixed_list/app.py
+++ exercises/04-mixed_list/app.py
@@ -1,6 +1,6 @@
 mix = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
-# Your code below
+# imprimir el tipo de cada elemento de la lista  
 def typeoflist(mixlist):
     for element in mixlist:
         print(type(element))
