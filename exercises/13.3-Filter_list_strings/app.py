f8212289bacba15cb1a7bdd56940bde505f9d750
Heidy Díaz <heidydiazbarrios@gmail.com>
2025-10-25 15:37:53 +0000

    loop 13

--- exercises/13.3-Filter_list_strings/app.py
+++ exercises/13.3-Filter_list_strings/app.py
@@ -1,3 +1,9 @@
 names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley']
 
-# Your code here
+# nombres que contienen am 
+def name_with_am(name):
+    if 'am' in name.lower():
+        return (name)
+    
+nombres_am = list(filter(name_with_am, names))
+print (nombres_am)
--- exercises/13.4-Making_HTML_with_filter_and_maP/app.py
+++ exercises/13.4-Making_HTML_with_filter_and_maP/app.py
@@ -9,4 +9,14 @@ all_colors = [
 ]
 
 # Your code here
+def filter_colors(color_sexy):
+    #color_s = [color_sexy['label'] for color_sexy in all_colors if color_sexy['sexy']]
+   # print (f'los colores sexy son: {color_s}')
+    return (color_sexy['sexy']) #devuelve ['Red', 'Orange', 'Pink', 'Violet']
+	
+def generate_li(color):
+    return (f'<li>{color["label"]}</li>')
 
+lista_sexy= list(filter(filter_colors, all_colors))  
+lista_sexy_con_html = list(map(generate_li, lista_sexy)) 
+print (lista_sexy_con_html)
